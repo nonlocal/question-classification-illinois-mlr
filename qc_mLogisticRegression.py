9288594f8863ea9ae446a6ffb0e640d40ea7de6e
nonlocal <c137nlc@gmail.com>
2016-06-22 19:27:45 +0530

    Create qc_mLogisticRegression.py

---
+++ qc_mLogisticRegression.py
@@ -0,0 +1,31 @@
+file = open('qc_data.txt','r')
+qc_dataset = file.read()
+qc_dataset = qc_dataset.split('\n')
+del qc_dataset[-1]
+index = len(qc_dataset)
+labels = []
+features = []
+for i in range(index):
+    labels.append(qc_dataset[i].split()[0])
+for i in range(index):
+    features.append(" ".join( qc_dataset[i].split()[1:] ))
+from sklearn.cross_validation import train_test_split
+train_feat, test_feat, train_label, test_label = train_test_split(features,labels,test_size=0.15)
+from sklearn.feature_extraction.text import CountVectorizer
+count_vect = CountVectorizer()
+train_vect = count_vect.fit_transform(train_feat)
+test_vect = count_vect.transform(test_feat)
+count = len(test_label)
+dummy=0
+##################################################
+#       LogisticRegression Multinomial
+##################################################
+from sklearn.linear_model import LogisticRegression
+clf_lr = LogisticRegression(multi_class='multinomial',solver='lbfgs')
+clf_lr = clf_lr.fit(train_vect, train_label)
+pr_lr = clf_lr.predict(test_vect)
+for i in range(count):
+    if(test_label[i]==pr_lr[i]):
+        dummy=dummy+1
+
+print(dummy, count, dummy/count)
